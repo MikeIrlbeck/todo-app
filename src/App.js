c449b147af2593ca9daa85bb19f50f725af20a91
MikeIrlbeck <91171891+MikeIrlbeck@users.noreply.github.com>
2023-02-25 10:21:42 -0700

    Added ability to add items to todo list, and to clear all items

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import React, { useState } from 'react';
 
 import './App.css';
@@ -8,33 +7,27 @@ function App() {
   const [items, setItems] = useState(data);
 
   const addItem = () => {
-    setItems(data.push({ id: data.length + 1, name: 'New item' }));
+    setItems([
+      ...items,
+      {
+        id: items.length + 1,
+        name: 'New todo',
+      },
+    ]);
   };
+
   return (
     <div className='App'>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <h1>TODO LIST</h1>
       <button onClick={addItem}>Add</button>
       <Item></Item>
       <Item></Item>
       <section className='itemList'>
-        {data.map((el) => {
+        {items.map((el) => {
           return <Item key={el.id} {...el}></Item>;
         })}
       </section>
+      <button onClick={() => setItems([])}>Clear</button>
     </div>
   );
 }
